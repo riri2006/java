25c2f0f3dfe3e17a37f109cdbba15a2a692e96db
Vedant Kapil <vedantkp79@gmail.com>
2026-08-11 22:14:08 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,41 +1,53 @@
- public class Shape{
-    public void findarea(){
+public class Shape {
+
+    public void findarea() {
         System.out.println("Area of shape...");
     }
-}
- class Circle extends Shape{
-    public void findarea(){
-        int radius =10;
-        System.out.println("Area of circle:"+ 3.14*radius*radius );
-    }
-}
-class Rectangle extends Shape{
-    public void findarea(){
-        int length = 10;
-        int breadth = 5;
-        System.out.println("Area of rectangle:"+ length*breadth);
-    }
-}
-class Triangle extends Shape{
-    public void findarea(){
-        int base = 10;
-        int height = 5;
-        System.out.println("Area of triangle:"+ 0.5*base*height);
-    }
-}
 
-class Main{
     public static void main(String[] args) {
+
         Shape s = new Shape();
         s.findarea();
+
         Circle c = new Circle();
         c.findarea();
+
         Rectangle r = new Rectangle();
         r.findarea();
+
         Triangle t = new Triangle();
         t.findarea();
     }
 }
 
 
+class Circle extends Shape {
 
+    public void findarea() {
+        int radius = 10;
+
+        System.out.println("Area of circle: " + 3.14 * radius * radius);
+    }
+}
+
+
+class Rectangle extends Shape {
+
+    public void findarea() {
+        int length = 10;
+        int breadth = 5;
+
+        System.out.println("Area of rectangle: " + length * breadth);
+    }
+}
+
+
+class Triangle extends Shape {
+
+    public void findarea() {
+        int base = 10;
+        int height = 5;
+
+        System.out.println("Area of triangle: " + 0.5 * base * height);
+    }
+}
