bc9d409359e299b89e86eb27ef7cef8fdbc17b6b
riri2006 <riddhiverma001@gmail.com>
2026-08-27 12:09:21 +0530

    array

---
+++ ArrayEg.java
@@ -0,0 +1,8 @@
+public class ArrayEg {
+    public static void main(String[] args) {
+        int[] arr = {5,10,15,20};
+        String[] name = {"Riddhi", "Vedant", "Kishu"};
+        System.out.println(arr[1]);
+        System.out.println(name[1]);
+    }
+}
