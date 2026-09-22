cc5cf0737b296e9f3ba64cc03d8843d471c4c595
riri2006 <riddhiverma001@gmail.com>
2026-09-22 12:00:57 +0530

    collection framework

---
+++ codes/CollectionEg.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionEg {
+    public static void main(String[] args) {
+        List <String> name = new ArrayList<>();
+        name.add("Riddhi");
+        name.add("Vedant");
+        name.add("Kishu");
+        System.out.println(name);
+        name.add("Abhishek");
+        System.out.println(name);
+    }
+}
